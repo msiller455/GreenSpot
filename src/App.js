ea08fdb7ebdd4cfe417c1811c66614fa65d79ace
Brent Ahrens <cidida@gmail.com>
2019-02-08 15:30:14 -0800

    Project 3: Splash component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<Splash/>
-
-      <Login />
+        <Splash/>
+        <Login/>
        
- master
       </div>
     );
   }
