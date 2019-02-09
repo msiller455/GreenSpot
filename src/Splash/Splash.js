8333b9e6f94c2310988979b873578acff2867dbf
Brent Ahrens <cidida@gmail.com>
2019-02-08 17:24:15 -0800

    brent: splash css updates

--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Splash = () => 
     <div className="splash-container">
-        <h1>Green Spot</h1>
+        <h1 className="splash-header">Green Spot</h1>
         <img className="splash-img" src="http://7gigzxopz0uiqxo1-zippykid.netdna-ssl.com/wp-content/uploads/2018/05/vegan-hooligans.jpg"></img>
         {/* <Link to='#'>Venue Login</Link> */}
     </div>
