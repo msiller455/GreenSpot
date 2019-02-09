4d6b84926abcb124881d8e6f62f7f9da2d86eb40
Brent Ahrens <cidida@gmail.com>
2019-02-08 21:31:13 -0800

    brent: react router is set up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import './App.css';
 import Splash from './Splash/Splash'
 import './Splash/Splash.css'
 import Button from './Button/Button'
 import './Button/Button.css'
 import Login from './Login/Login'
+import { Route, Switch } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <Splash/>
-      <Button/>
-      </div>
-    );
-  }
+const my404 = () => {
+  return(
+    <div>
+      You are Lost!
+    </div>
+  )
+}
+const App = () => {
+  return(
+    <main>
+      <Switch>
+        <Route exact path='/' component={ Splash } />
+        <Route component={ 404 }/>
+      </Switch>
+    </main>
+  )
 }
 
 export default App;
--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Button from '../Button/Button'
 import { Link } from 'react-router-dom'
 
 const Splash = () => 
     <div className="splash-container">
         <h1 className="splash-header">Green Spot</h1>
         <img className="splash-img" src="http://7gigzxopz0uiqxo1-zippykid.netdna-ssl.com/wp-content/uploads/2018/05/vegan-hooligans.jpg"></img>
-        {/* <Link to='#'>Venue Login</Link> */}
+        <Button/>
+        <Link to='#'>Venue Login</Link>
     </div>
 
 export default Splash 
