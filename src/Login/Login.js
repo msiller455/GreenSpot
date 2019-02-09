ce95e0bcabd8c88cb48ffeaad4dcdbdb0868a46a
Brent Ahrens <cidida@gmail.com>
2019-02-09 15:50:42 -0800

    brent: made sign up route

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -15,14 +15,13 @@ class Login extends Component {
         return (
             <div className="login-container">
                 <h1 className="login-header">Log in</h1>
-                <h4 className="login-header">New to Green Spot? <Link className="signup-link" to='/login'>Sign up</Link></h4>
+                <h4 className="login-header">New to Green Spot? <Link className="signup-link" to='/signup'>Sign up</Link></h4>
                 <form className="forms">
                     <input className="login-email" type="email" name="email" placeholder="Email"/>
                     <input className="login-password" type="password" name="password" placeholder="Password"/><br></br>
                     <button className="login-btn" onClick={this.handleLogin}>Log in</button>
                 </form>
             </div>
-
         )
     }
 
