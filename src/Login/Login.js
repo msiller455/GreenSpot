e0c6cbad840a0d3857459261d430aef4db60c3fe
Brent Ahrens <cidida@gmail.com>
2019-02-09 13:28:12 -0800

    brent: added header to login page

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Login extends Component {
     state = {
@@ -13,10 +14,13 @@ class Login extends Component {
     render () {
         return (
             <div>
+                <h1 className="login-header">Log in</h1>
+                <h4 className="login-header">New to Green Spot?</h4>
+               
                 <form className="forms">
                     <input className="login-email" type="email" name="email" placeholder="Email"/>
                     <input className="login-password" type="password" name="password" placeholder="Password"/><br></br>
-                    <button className="login-btn" onClick={this.handleLogin}>Login</button>
+                    <button className="login-btn" onClick={this.handleLogin}>Log in</button>
                 </form>
             </div>
 
