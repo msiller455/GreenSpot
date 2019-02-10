af5a5198889dede93b8df1580f3ca28926bf6136
Brent Ahrens <cidida@gmail.com>
2019-02-10 14:22:10 -0800

    brent: made a new branch show-page

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -15,7 +15,7 @@ class SingUp extends Component {
     render () {
         return (
             <div className="signup-container">
-                <img className="vegan-logo" src="https://www.v-label.eu/wp-content/uploads/2016/10/allergien2.png" height="70px" width="70px"></img>
+                <img className="vegan-logo" src="https://www.v-label.eu/wp-content/uploads/2016/10/allergien2.png" height="45px" width="45px"></img>
                 <h1 className="signup-main-header">Sign up</h1>
                 <h4 className="signup-header">Already a member?<Link className="signup-link" to='/login'> Log in</Link></h4>
                 <form className="forms">
