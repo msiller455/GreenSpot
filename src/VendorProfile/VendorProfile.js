c05d55fe69350f07fc68cb082f22f4500c39b1b0
Brent Ahrens <cidida@gmail.com>
2019-02-11 12:58:55 -0800

    brent: changed nav bar names

--- src/VendorProfile/VendorProfile.js
+++ src/VendorProfile/VendorProfile.js
@@ -8,12 +8,7 @@ class VendorProfile extends Component {
     render () {
         return (
             <div className="show-container">
-                <header><NavBar/></header>
-                <div className="show-flex">
-                    <SearchBar/>
-                </div>
-                    <Maps/>
-                <footer><Footer/></footer>
+     
             </div>
         )
     }
