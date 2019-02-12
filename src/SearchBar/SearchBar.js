832fb068e57ea5d651af9c651c3c1a4b58581eec
Abraham Hsu <smokiebacon@gmail.com>
2019-02-11 16:26:14 -0800

    fetch google api

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+
 class SearchBar extends Component {
     state = {
-        searchInput: ''
+        searchInput: '',
+        geoAddress: ''
     }
+       
     handleChange = (e) => {
         console.log(e.target.value)
         this.setState({
@@ -16,16 +19,40 @@ class SearchBar extends Component {
         this.setState({
             searchInput : e.target.value
         })
-        console.log(this.state);
+        console.log(this.state.searchInput);
+    //      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.searchInput}&key=AIzaSyCzdgTlTndmIPFlvVcelpUoYWykNd7Qq4o`).then(res=>res.json()).then(function(data) {
+    //  console.log(data)})
         //search the area for vendors
         //populate area with vendor markers. //with markers have vendor show info
     }
+  
+  
+
+      getGeoaddress = async () => {
+        try { 
+        const geoAddress = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.searchInput}&key=AIzaSyCzdgTlTndmIPFlvVcelpUoYWykNd7Qq4o`);
+        if(!geoAddress.ok) {
+          throw Error(geoAddress.statusText);
+        }
+        const geoAddressParsedJson = await geoAddress.json();
+        console.log(geoAddressParsedJson, 'THIS IS PARSED');
+        this.setState({
+          geoAddress: geoAddressParsedJson.data
+        })
+      } catch (err) {
+        console.log(err, 'error in catch')
+      }
+    }
+    
+    componentDidMount () {
+        this.getGeoaddress();  
+    } 
 
 
     render () {
         return (
             <div className="search-container">
-                <input className="search-input" onChange={this.handleChange} type="text" placeholder="Enter your address"></input>
+                <input name="searchInput"className="search-input" onChange={this.handleChange} type="text" placeholder="Enter your address"></input>
                <button onClick={this.handleSearch}>Search </button>
             </div>
         )
