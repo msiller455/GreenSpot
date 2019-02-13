18b9e192848dbcd73cc78d69243ca763a0919c51
Brent Ahrens <cidida@gmail.com>
2019-02-13 13:42:58 -0800

    brent: update on edit page and show page

--- src/VendorProfile/VendorProfile.js
+++ src/VendorProfile/VendorProfile.js
@@ -9,10 +9,12 @@ import {withRouter} from 'react-router-dom'
 
 class VendorProfile extends Component {
     state = {
-        fileSelected: null
+        fileSelected: null,
+        vendor: {}
     }
 
     componentDidMount() {
+        console.log('vendor mounted')
         axios(`/users/${this.props.match.params.id}`)
             .then(res => {
                 this.setState({
@@ -28,6 +30,7 @@ class VendorProfile extends Component {
         })
         console.log(e.target.files[0])
     }
+
     fileUpload = () => {
         const fd = new FormData();
         fd.append('image', this.state.fileSelected, this.state.fileSelected.name)
@@ -37,22 +40,14 @@ class VendorProfile extends Component {
             })
 
     }
+
     routeChange = () => {
-     this.props.history.push(`/users/${this.props.match.params.id}/edit`);
+        this.props.history.push(`/users/${this.props.match.params.id}/edit`);
     }
 
- vendorUpdate = async (e) => {
-     e.preventDefault()
-     axios.put('/users/${this.props.match.params.id}', this.state)
-    //  try {
-    //     const response = await axios(`/users/${this.props.match.params.id}`, {
-    //         method: 'PUT',
-    //         credentials: 'include',
-    //         body: JSON.stringify(this.state.)
-    //     })
-    //  } catch (err) {
-    //      console.log(err)
-    //  }
+    vendorUpdate = async (e) => {
+        e.preventDefault()
+        axios.put('/users/${this.props.match.params.id}', this.state)
     }
 
 
@@ -61,23 +56,18 @@ class VendorProfile extends Component {
         return (
             <div className="show-container">
                 <NavBar/><br></br>
-                <h1 className="vendor-show-name">The Vegan Hooligans</h1>
+                <h1 className="vendor-show-name">{this.state.vendor.vendorName}</h1>
                 <button onClick={this.routeChange} vendorUpdate={this.vendorUpdate} >Edit Profile</button>
                 <div className="vendor-img-bio-flex">
-                    <img className="vendor-show-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9KlOpbuI65oPgH5O7IisnrPTQ4XqZnFZ8sfHLwEZldPg0Y5ha" height="120rem"></img>
-                    <h3 className="vendor-bio">We make vegan food fun. , we love giving ya'll them spicy pickles you love!
-                        make sure to try our new Mac Melt sandwhich.
-                    </h3>
+                    <img className="vendor-show-logo" src="{this.state.image}" height="120rem"></img>
+                    <h3 className="vendor-bio">{this.state.vendor.location}</h3>
                 </div>
                     <Carousel>
                         <img className="cara-img" src="http://7gigzxopz0uiqxo1-zippykid.netdna-ssl.com/wp-content/uploads/2018/05/vegan-hooligans.jpg" />
                         <img className="cara-img" src="https://images.happycow.net/venues/1024/11/54/hcmp115492_375128.jpeg" />
                         <img className="cara-img" src="https://static.wixstatic.com/media/afa30d_a284a182491549e0af7dd6b678cd6066~mv2_d_2400_1602_s_2.jpg" />
                     </Carousel>
-                    <h1 className="vendor-show-review">Reviews</h1>
-                    <h3 className="user-reviews">Brent: This place is bomb!!! My favorite spot in the York Zone. Highly recommand.
-                        this is a must for anyone new to the vegan street food world.
-                    </h3><br></br><br></br>
+                    <h2 className="vendor-show-review">{this.state.vendor.website}</h2>
 
                 <Footer/>
 
