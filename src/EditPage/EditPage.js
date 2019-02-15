383bd64a6bbff7417e83b0f1004cc738c32680e0
Michael <msiller455@gmail.com>
2019-02-14 21:08:17 -0800

    fixes the edit route

--- src/EditPage/EditPage.js
+++ src/EditPage/EditPage.js
@@ -6,11 +6,10 @@ class EditPage extends Component {
 
     vendorUpdate = (e) => {
         e.preventDefault()
-        console.log(this.props.user)
-        axios.put(`/users/${this.props.user.id}`, this.state)
+        axios.put(`${process.env.REACT_APP_API_URL}/users/${this.props.user.id}`, this.state)
             .then(res => {
                 (res.status === 200)
-                    ? this.props.history.push(`${process.env.REACT_APP_API_URL}/users/${this.props.user.id}`)
+                    ? this.props.history.push(`/users/${this.props.user.id}`)
                     : console.log(res.error)
             })
     }
